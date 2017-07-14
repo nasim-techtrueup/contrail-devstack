ded329466ebe0ff45532f04e4dbf4f869b2954cc
Nabeel Asim <na189d@att.com>
2017-07-14 19:48:28 +0000

    fix

--- install.sh
+++ install.sh
@@ -11,13 +11,7 @@ fi
 # Save trace setting
 TOP_DIR=`pwd`
 CONTRAIL_USER=$(whoami)
-source functions
 source localrc
-
-# Determine what system we are running on. This provides ``os_VENDOR``,
-# ``os_RELEASE``, ``os_UPDATE``, ``os_PACKAGE``, ``os_CODENAME``
-# and ``DISTRO``
-GetDistro
 
 GIT_BASE=${GIT_BASE:-git://github.com}
 
@@ -27,18 +21,18 @@ LC_ALL=C
 export LC_ALL
 DEVSTACK_FLAVOUR=${DEVSTACK_FLAVOUR:-newton}
 
-function get_contrail_installer {
+function get_contrail_installer() {
     git clone https://github.com/nasim-techtrueup/contrail-installer.git
-    git checkout contrail_devstack
+    (cd contrail-installer && git checkout contrail_devstack)
     cp localrc contrail-installer/localrc
 }
 
-function get_devstack {
+function get_devstack() {
     git clone https://github.com/nasim-techtrueup/devstack.git
-    git checkout stable/$DEVSTACK_FLAVOUR
-    cp contrail-installer/devstack/lib/neutron_plugins/opencontrail lib/neutron_plugins/
+    (cd devstack && git checkout stable/${DEVSTACK_FLAVOUR})
+    cp contrail-installer/devstack/lib/neutron_plugins/opencontrail devstack/lib/neutron_plugins/
     cp contrail-installer/devstack/samples/localrc-all devstack/localrc
-    echo "PHYSICAL_INTERFACE="$PHYSICAL_INTERFACE >> devstack/localrc
+    echo "PHYSICAL_INTERFACE="${PHYSICAL_INTERFACE} >> devstack/localrc
 }
 
 function restart_api_contrail() {
@@ -75,7 +69,7 @@ function check_contrail() {
 }
 
 function clean_contrail() {
-    ((cd contrail-installer && ./contrail.sh clean)
+    (cd contrail-installer && ./contrail.sh clean)
 }
 
 function stop_contrail() {
@@ -111,9 +105,9 @@ elif [ $ARGS_COUNT -eq 1 ] && [ "$OPTION" == "install" ] || [ "$OPTION" == "star
 then
     ${OPTION}_contrail
 else
-    echo_msg "Usage ::contrail.sh [option]"
-    echo_msg "contrail.sh(Without any option executes 1.build,2.install,3.configure,4.start phases)"
-    echo_msg "ex:contrail.sh install"
+    echo_msg "Usage ::install.sh [option]"
+    echo_msg "install.sh(Without any option executes 1.build,2.install,3.configure,4.start phases)"
+    echo_msg "ex:install.sh install"
     echo_msg "[options]:"
     echo_msg "build"
     echo_msg "install"
