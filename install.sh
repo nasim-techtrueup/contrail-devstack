c1d341817dc0249fd0001890d2716281b33487a7
Nabeel Asim <na189d@att.com>
2017-07-16 15:37:44 +0000

    Add handling of events

--- install.sh
+++ install.sh
@@ -21,43 +21,117 @@ LC_ALL=C
 export LC_ALL
 DEVSTACK_FLAVOUR=${DEVSTACK_FLAVOUR:-newton}
 
+function wait_for_contrail_task() {
+    file_count=`ls .stage/contrail | wc -l`
+    while [ $file_count -ne 0 ]; do
+        echo "--------------Waiting for contrail task completion------------"
+        sleep 10
+        file_count=`ls .stage/contrail | wc -l`
+    done
+}
+
 function get_contrail_installer() {
+    sudo rm -rf contrail-installer
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     git clone https://github.com/nasim-techtrueup/contrail-installer.git
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     (cd contrail-installer && git checkout contrail_devstack)
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     cp localrc contrail-installer/localrc
+    echo "REMOVE_FILE_ON_COMPLETION=../.stage/contrail" >> contrail-installer/localrc
+    echo 1 > .stage/get_contrail
 }
 
 function get_devstack() {
+    sudo rm -rf devstack
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     git clone https://github.com/nasim-techtrueup/devstack.git
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     (cd devstack && git checkout stable/${DEVSTACK_FLAVOUR})
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     cp contrail-installer/devstack/lib/neutron_plugins/opencontrail devstack/lib/neutron_plugins/
     cp contrail-installer/devstack/samples/localrc-all devstack/localrc
     echo "PHYSICAL_INTERFACE="${PHYSICAL_INTERFACE} >> devstack/localrc
+    echo 1 > .stage/get_devstack
 }
 
 function restart_api_contrail() {
     (cd contrail-installer && ./contrail.sh restart_api)
+    status=$?
+    wait_for_contrail_task
+    if [ $status -ne 0 ]; then
+        exit 1
+    fi
 }
 
 function build_contrail() {
     (cd contrail-installer && ./contrail.sh build)
+    status=$?
+    wait_for_contrail_task
+    if [ $status -ne 0 ]; then
+        exit 1
+    fi
 }
 
 function install_contrail() {
     (cd contrail-installer && ./contrail.sh install)
+    status=$?
+    wait_for_contrail_task
+    if [ $status -ne 0 ]; then
+        exit 1
+    fi
 }
 
 function restart_contrail() {
-    (cd contrail-installer && ./contrail.sh restart)
+    echo "Restart function not supported"
+    #(cd contrail-installer && ./contrail.sh restart)
+    #status=$?
+    #wait_for_contrail_task
+    #if [ $status -ne 0 ]; then
+    #    exit 1
+    #fi
 }
 
 function start_contrail() {
     (cd contrail-installer && ./contrail.sh start)
+    status=$?
+    wait_for_contrail_task
+    if [ $status -ne 0 ]; then
+        exit 1
+    else
+        echo "-----------------------------------------------------------"
+        echo "---------------contrail start completed--------------------"
+        echo "-----------------------------------------------------------"
+    fi
     (cd devstack && ./stack.sh)
+    if [ $? -ne 0 ]; then
+        exit 1
+    else
+        echo "-----------------------------------------------------------"
+        echo "---------------Devstack stack completed--------------------"
+        echo "-----------------------------------------------------------"
+    fi
 }
 
 function configure_contrail() {
     (cd contrail-installer && ./contrail.sh configure)
+    status=$?
+    wait_for_contrail_task
+    if [ $status -ne 0 ]; then
+        exit 1
+    fi
 }
 
 function init_contrail() {
@@ -70,29 +144,78 @@ function check_contrail() {
 
 function clean_contrail() {
     (cd contrail-installer && ./contrail.sh clean)
+    status=$?
+    wait_for_contrail_task
+    if [ $status -ne 0 ]; then
+        exit 1
+    fi
 }
 
 function stop_contrail() {
-    (cd contrail-installer && ./contrail.sh stop)
     (cd devstack && ./unstack.sh)
+    if [ $? -eq 0 ];
+    then
+        echo "-----------------------------------------------------------"
+        echo "---------------Devstack unstack completed------------------"
+        echo "-----------------------------------------------------------"
+    else
+        exit 1
+    fi
+    echo 1 > .stage/contrail
+    (cd contrail-installer && ./contrail.sh stop)
+    status=$?
+    wait_for_contrail_task
+    if [ $status -eq 0 ];
+    then
+        echo "-----------------------------------------------------------"
+        echo "---------------contrail stop completed---------------------"
+        echo "-----------------------------------------------------------"
+    else
+        exit 1
+    fi
 }
 
 function all_contrail_devstack() {
+    echo 1 > .stage/contrail
     (cd contrail-installer && ./contrail.sh)
+    status=$?
+    wait_for_contrail_task
+    if [ $status -eq 0 ];
+    then
+        echo "-----------------------------------------------------------"
+        echo "---------------contrail start completed--------------------"
+        echo "-----------------------------------------------------------"
+    else
+        exit 1
+    fi
     (cd devstack && ./stack.sh)
+    if [ $? -eq 0 ];
+    then
+        echo "-----------------------------------------------------------"
+        echo "---------------Devstack stack completed--------------------"
+        echo "-----------------------------------------------------------"
+    else
+        exit 1
+    fi
 }
 
 #=================================
 OPTION=$1
 ARGS_COUNT=$#
 
-have_contrail_installer=`find . -name contrail-installer | grep ./contrail-installer | wc -l`
+HAVE_STAGE_DIR=`ls -la | grep .stage | wc -l`
+if [ $HAVE_STAGE_DIR -eq 0 ];
+then
+    mkdir .stage
+fi
+
+have_contrail_installer=`ls -l .stage/get_contrail | wc -l`
 if [ $have_contrail_installer -eq 0 ];
 then
     get_contrail_installer
 fi
 
-have_devstack=`find . -name contrail-installer | grep ./devstack | wc -l`
+have_devstack=`ls -l .stage/get_devstack | wc -l`
 if [ $have_devstack -eq 0 ];
 then
     get_devstack
